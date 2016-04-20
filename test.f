c8c4b73b54b21689a57029416bfa1c2ded41af52
Qi Liu <liuq901@163.com>
2016-04-20 14:33:13 +0800

    update new testcase

--- test.f
+++ test.f
@@ -22,3 +22,5 @@ if (lambda x. x) true then 1 else 2;
 (lambda x. if x then true else false) false;
 
 (lambda x. x (lambda x. x)) 1;
+
+(lambda x. lambda y. y x) 26 (lambda z. if iszero z then 20 else 23);
